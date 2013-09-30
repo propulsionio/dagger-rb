814d285096f41c7208a0a9685f29335708d2316c
Karl Jonathan Ward <karl.j.ward@gmail.com>
2013-09-30 18:00:47 +0100

    Expose branding information for UI.

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'yaml'
+require 'json'
 
 require_relative 'lib/configuration'
 require_relative 'lib/aggregate'
@@ -20,14 +21,21 @@ configure do
   config = load_configuration("conf.yaml")
   prepare_database(config)
   prepare_schedule(config)
+
+  set :branding, config['branding']
 end
 
 get '/tallies' do
   content_type 'application/json'
-  fetch_tallies
+  fetch_tallies.to_json
 end
 
 get '/breakdowns' do
   content_type 'application/json'
-  fetch_breakdowns
+  fetch_breakdowns.to_json
+end
+
+get '/branding' do
+  content_type 'application/json'
+  settings.branding.to_json
 end
